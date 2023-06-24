b972123b33ab86c2f62414681b242c31a1d91986
samriddhiK <75959569+samriddhiK@users.noreply.github.com>
2023-06-24 15:09:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+prediction_1 = ""
+prediction_2 = ""
+
+Webcam.set({
+    width:350,
+    height: 300,
+    image_format: 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'; 
+    });
+}
+
+console.log('ml5 version:', ml5.version);  //here is 21
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/eotjgY04E/model.json ',modelLoaded);
+
+function speak()
+{ 
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "And the second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis); 
+} 
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult); //here is 36
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+    console.log(results);
+    document.getElementById("result_emoji_name").innerHTML = results[0].label;
+    document.getElementById("result_emoji_name2").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+    if(results[0].label == "thumbs-up")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#128522;";
+    }
+    if(results[0].label == "thumbs-down")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#128532;";
+    }
+    if(results[0].label == "rockin`")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#128532;";
+    }
+    
+    if(results[1].label == "thumbs-up")
+    {
+        document.getElementById("update_emoji2").innerHTML = "&#128522;";
+    }
+    if(results[1].label == "thumbs-down")
+    {
+        document.getElementById("update_emoji2").innerHTML = "&#128532;";
+    }
+    if(results[1].label == "rockin`")
+    {
+        document.getElementById("update_emoji2").innerHTML = "&#128548;";
+    }
+    }
+}
+
+
+
